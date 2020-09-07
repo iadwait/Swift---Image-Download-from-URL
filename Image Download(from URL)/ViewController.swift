7374673d20abdd67d38e40d662cc646dc67d072f
iAdwait <adwaitbarkale@gmail.com>
2020-09-07 10:57:22 +0530

    Initial Commit

---
+++ Image Download(from URL)/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Image Download(from URL)
+//
+//  Created by Adwait Barkale on 07/09/20.
+//  Copyright © 2020 Adwait Barkale. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class ViewController: UIViewController {
+    
+    let strImageUrl = "https://iosacademy.io/assets/images/brand/icon.jpg"
+    let imageView: UIImageView = {
+        let iv = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        iv.backgroundColor = .gray
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imageView.center = view.center
+        view.addSubview(imageView)
+        fetchImage(urlStr: strImageUrl)
+        //imageView.sd_setImage(with: URL(string: strImageUrl), completed: nil)
+    }
+    
+    func fetchImage(urlStr: String)
+    {
+        let url = URL(string: urlStr)
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url!) { (data, _, error) in
+            guard let imgData = data,error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                let image = UIImage(data: imgData)
+                self.imageView.image = image
+            }
+            
+        }
+        task.resume()
+    }
+    
+}
+
